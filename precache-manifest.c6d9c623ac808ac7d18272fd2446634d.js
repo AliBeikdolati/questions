c57d44bd37549bd647bd5bea1f7507621ab78947
ali beik dolati <ali.beik2lati@gmail.com>
2021-02-18 12:21:25 +0330

    Updates

---
+++ precache-manifest.c6d9c623ac808ac7d18272fd2446634d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83705513944455627e3",
+    "url": "/questions/css/app.8e47e505.css"
+  },
+  {
+    "revision": "a662c1b6e80bd8ce0a6f",
+    "url": "/questions/css/chunk-vendors.78bb8988.css"
+  },
+  {
+    "revision": "ddf1ee764fb9b3d3ba6bc217d00a3ea4",
+    "url": "/questions/index.html"
+  },
+  {
+    "revision": "a588b3247303cff84463",
+    "url": "/questions/js/about.85fcb712.js"
+  },
+  {
+    "revision": "b83705513944455627e3",
+    "url": "/questions/js/app.f45967dd.js"
+  },
+  {
+    "revision": "a662c1b6e80bd8ce0a6f",
+    "url": "/questions/js/chunk-vendors.d2902e09.js"
+  },
+  {
+    "revision": "bd922014b78138b27721839f9a53b4ca",
+    "url": "/questions/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/questions/robots.txt"
+  }
+]);
